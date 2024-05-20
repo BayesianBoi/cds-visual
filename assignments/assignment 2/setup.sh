cf8b53eb1e271dc64053de1940832072499bbc0d
Niels Værbak <98818462+BayesianBoi@users.noreply.github.com>
2024-05-20 21:16:10 +0200

    Add files via upload

---
+++ assignments/assignment 2/setup.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Create a virtual environment
+python3 -m venv envVis2
+
+# Activate the virtual environment
+source envVis3/bin/activate
+
+# Upgrade pip
+pip install --upgrade pip
+
+# Install required packages
+pip install -r requirements.txt
+
+# Deactivate the virtual environment
+deactivate

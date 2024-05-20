35d036b24c0e9bd7b8f749734a463ebbbeff6af0
Niels Værbak <98818462+BayesianBoi@users.noreply.github.com>
2024-05-20 18:50:22 +0200

    Update comparison.py
    more documentation

--- assignments/assignment 1/src/comparison.py
+++ assignments/assignment 1/src/comparison.py
@@ -1,17 +1,17 @@
 import pandas as pd
 
 def compare_results():
-    # Load results from Image Search Script
-    img_search_csv = f"../out/hist_image_0001.csv"
+    # Load results from colour hist model
+    img_search_csv = f"../out/hist_image_0001.jpg.csv"
     img_search_df = pd.read_csv(img_search_csv)
-    img_search_top5 = set(img_search_df['Filename'].iloc[1:6])  # Skip the first row (chosen image itself)
+    img_search_top5 = set(img_search_df["Filename"].iloc[1:6])  # skipping the first row (which is the chosen image itself)
 
-    # Load results from VGG16 Script
-    vgg16_csv = f"../out/vgg16_image_0001.csv"
+    # loading the results from vgg16 model
+    vgg16_csv = f"../out/vgg16_image_0001.jpg.csv"
     vgg16_df = pd.read_csv(vgg16_csv)
-    vgg16_top5 = set(vgg16_df['Filename'].iloc[1:6])  # Skip the first row (chosen image itself)
+    vgg16_top5 = set(vgg16_df["Filename"].iloc[1:6])  # skipping the first row (which is the chosen image itself)
 
-    # Compare results
+    # comparing the results to see if any of the predicted pictures overlap
     common_images = img_search_top5.intersection(vgg16_top5)
     print(f"Common similar images for {chosen_image_filename}: {common_images}")
     print(f"Number of common similar images: {len(common_images)}")

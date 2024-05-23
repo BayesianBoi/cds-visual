3df1f970a7369f38d2aa7cb0fea68917ed61a2ca
Niels Værbak <98818462+BayesianBoi@users.noreply.github.com>
2024-05-23 12:13:57 +0200

    Update comparison.py
    updated filepaths

--- assignments/assignment 1/src/comparison.py
+++ assignments/assignment 1/src/comparison.py
@@ -2,12 +2,12 @@ import pandas as pd
 
 def compare_results():
     # Load results from colour hist model
-    img_search_csv = "../out/hist_image_0001.jpg.csv"
+    img_search_csv = "out/hist_image_0001.jpg.csv"
     img_search_df = pd.read_csv(img_search_csv)
     img_search_top5 = set(img_search_df["Filename"].iloc[1:6])  # taking the top five most similar images while skipping the first row (which is the chosen image itself)
 
     # loading the results from vgg16 model
-    vgg16_csv = "../out/vgg16_image_0001.jpg.csv"
+    vgg16_csv = "out/vgg16_image_0001.jpg.csv"
     vgg16_df = pd.read_csv(vgg16_csv)
     vgg16_top5 = set(vgg16_df["Filename"].iloc[1:6])  # taking the top five most similar images while skipping the first row (which is the chosen image itself)
 
